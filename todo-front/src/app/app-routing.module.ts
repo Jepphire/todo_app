6c7d32aa0186bded434d85605fc34224d9cd6bbd
Jepphire <90650560+Jepphire@users.noreply.github.com>
2022-08-12 17:44:35 -0500

    auth guard for protected routes

--- todo-front/src/app/app-routing.module.ts
+++ todo-front/src/app/app-routing.module.ts
@@ -2,14 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AuthGuard } from './shared/services/auth.guard.service';
 import { TodoFormComponent } from './todo-form/todo-form.component';
 import { TodoComponent } from './todo/todo.component';
 
 const routes: Routes = [
   { path: '*', redirectTo: '' },
   { path: '', pathMatch: 'full', component: HomeComponent },
-  { path: 'todo/:id', pathMatch: 'full', component: TodoComponent },
-  { path: 'todos/new', pathMatch: 'full', component: TodoFormComponent }
+  { path: 'todo/:id', pathMatch: 'full', component: TodoComponent, canActivate: [AuthGuard] },
+  { path: 'todos/new', pathMatch: 'full', component: TodoFormComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
